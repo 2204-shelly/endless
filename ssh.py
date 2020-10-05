045a721fbbdeacb996172ff6cb178d7580548182
Ambrose Chua <ambrose@hey.com>
2020-10-05 11:22:20 +0800

    Improve SSH timings

--- ssh.py
+++ ssh.py
@@ -6,9 +6,9 @@ import random
 async def handler(_reader, writer):
     try:
         while True:
-            await asyncio.sleep(10)
             writer.write(b'%x\r\n' % random.randint(0, 2**32))
             await writer.drain()
+            await asyncio.sleep(5)
     except ConnectionResetError:
         pass
 
